30c372ff92faa5030451f6507038ed0d9eaf5da0
Diógenes Oliveira <diogenes1oliveira@gmail.com>
2019-09-01 12:52:00 -0300

    Wrapper functions for mongo utils (#3)
    * [mongo] adding utilities to deal with Mongo connection settings
    
    * [aws] injection of AWS SSM Parameters
    
    * [quality] fixing Flake8 complaints
    
    * [tests] enabling doctests in the pytest.ini file
    
    * [tests] properly specifying the doctests
    
    * [aws_utils] minor whitespace in the docstring
    
    * [mongo_utils] downloading
    
    * [tests] making flake8 happy
    
    * forgot a debugging statement
    
    * [multipart] uploading from stream
    
    * [mongo_utils] functions to deal with mongodump and mongorestore
    
    * [mongo_utils] adding typing information

--- lambda_mongo_utils/mongo_utils.py
+++ lambda_mongo_utils/mongo_utils.py
@@ -4,11 +4,75 @@
 Miscellaneous utilities to interface with Mongo.
 '''
 
+from contextlib import contextmanager
+from dataclasses import dataclass
+import logging
+import os
+from pathlib import Path
 import re
+from shlex import split as shell_split
+import subprocess
 import urllib.parse
+import tarfile
+import tempfile
+import time
+from typing import (
+    Any,
+    BinaryIO,
+    ContextManager,
+    Iterable,
+    Mapping,
+    NamedTuple,
+    Optional,
+)
+
+from bson.objectid import ObjectId
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(os.getenv('LOG_LEVEL', 'INFO'))
 
 
-def parse_uri(uri):
+URL_DOWNLOAD_BASE = (
+    'http://downloads.mongodb.org/linux/mongodb-linux-x86_64-amazon2-v'
+)
+AVAILABLE_MONGO_UTILS = [
+    'bsondump',
+    'install_compass',
+    'mongo',
+    'mongod',
+    'mongodump',
+    'mongoexport',
+    'mongofiles',
+    'mongoimport',
+    'mongoreplay',
+    'mongorestore',
+    'mongos',
+    'mongostat',
+    'mongotop',
+]
+
+
+@dataclass
+class MongoStats:
+    collection: str = None
+    db: str = None
+    num_docs: int = None
+    time: float = None
+    duplicated_ids: Iterable[str] = None
+
+    @contextmanager
+    def measure(self):
+        t0 = time.time()
+        yield
+        self.time = time.time() - t0
+
+
+class MongoDumpOutput(NamedTuple):
+    stream: BinaryIO
+    stats: MongoStats
+
+
+def parse_uri(uri: str) -> Mapping[str, Optional[str]]:
     '''
     Parses a Mongo connection URI, returnining a dictionary with at least
     the following key:
@@ -62,7 +126,7 @@ def parse_uri(uri):
     return result
 
 
-def get_cmd_args(uri):
+def get_cmd_args(uri: str) -> Iterable[str]:
     '''
     Builds the list of command-line arguments to connect to the database
     via Mongo utils.
@@ -93,3 +157,218 @@ def get_cmd_args(uri):
         args += ['--user', parts['user'], '--password', parts['pwd']]
 
     return args
+
+
+def download_utils(
+    dest: str = '/tmp/bin',
+    version: str = '4.0-latest',
+    utils: str = None,
+):
+    '''
+    Downloads a Mongo version and extracts the specified binaries.
+
+    Args:
+    - dest: destination directory (created if it doesn't exist)
+    - version: version of Mongo to be downloaded (default: 4.0)
+    - utils: utils to be downloaded (default: ['mongo', 'mongodump'])
+
+    Returns
+        {MONGO_UTIL_NAME: PATH}
+
+    The names for the versions can be found in the URL
+    https://www.mongodb.org/dl/linux/x86_64-amazon2
+    '''
+    url = f'{URL_DOWNLOAD_BASE}{version}.tgz'
+    dest = Path(dest)
+    utils = ['mongo', 'mongodump'] if utils is None else utils
+
+    if dest.exists() and not dest.is_dir():
+        raise Exception('Destination is not a directory')
+
+    dest.mkdir(parents=True, exist_ok=True)
+    utils_to_return = {}
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        temp_tgz = Path(tmpdir) / 'mongo.tgz'
+
+        LOGGER.info('Downloading %s', url)
+        urllib.request.urlretrieve(url, temp_tgz)
+        LOGGER.info('Downloaded to %s', temp_tgz)
+
+        with tarfile.open(temp_tgz) as tar:
+            member_names = tar.getnames()
+
+            for util in utils:
+                util_dest_path = Path(dest) / util
+
+                try:
+                    member_name = [
+                        m for m in member_names
+                        if m.endswith(f'bin/{util}')
+                    ][0]
+                except IndexError:
+                    raise ValueError(f'No such util {util} in the file')
+                else:
+                    LOGGER.info('Extracting %s to %s', util, util_dest_path)
+                    with util_dest_path.open('wb') as fp:
+                        fp.write(tar.extractfile(member_name).read())
+
+                    LOGGER.info('Adding chmod +x to %s', util_dest_path)
+                    util_dest_path.chmod(0o755)
+
+                    utils_to_return[util] = str(util_dest_path)
+
+            if not utils:
+                LOGGER.info('No util to extract')
+
+    return utils_to_return
+
+
+@contextmanager
+def mongo_dump(
+    uri: str,
+    collection: str,
+    db: str = None,
+    query: Mapping[str, Any] = None,
+    buffer_size=None,
+    count=True,
+    cmd_prefix: str = '',
+) -> ContextManager[MongoDumpOutput]:
+    '''
+    Executes mongodump, yielding a stream to read its output.
+
+    Args:
+    - uri: Mongo connection string
+    - collection: name of the collection to be dumped
+    - db: name of the database (defaults to the one in the URI or 'admin')
+    - query: query to select the documents to be dumped
+    - buffer_size: size of the buffer for the stdout (default: 10MB)
+    - count: count the number of documents
+    - cmd_prefix: prefix to be added to the mongodump base command
+
+    Yields:
+        MongoDumpOutput
+    '''
+    buffer_size = buffer_size or 10_000_000
+
+    parts = parse_uri(uri)
+    db = db or parts.get('db', 'admin')
+    stats = MongoStats(db=db, collection=collection)
+
+    args = shell_split(cmd_prefix + 'mongodump')
+    args += get_cmd_args(uri)
+    args += [
+        '--db', db,
+        '--collection', collection,
+        '--archive', '--gzip',
+    ]
+    try:
+        process = subprocess.Popen(
+            args,
+            errors=None,
+            stdout=subprocess.PIPE,
+            stderr=(subprocess.PIPE if count else subprocess.DEVNULL),
+            text=None,
+            encoding=None,
+            universal_newlines=None,
+            bufsize=buffer_size,
+        )
+
+        with stats.measure():
+            yield MongoDumpOutput(
+                stream=process.stdout,
+                stats=stats,
+            )
+        _, stderr = process.communicate(timeout=1.0)
+        stderr = stderr.decode('utf-8') if stderr else ''
+
+        if process.returncode != 0:
+            LOGGER.error(stderr)
+            raise Exception(
+                f'mongodump exited with error code = {process.returncode}')
+
+        m = re.search(
+            f'done dumping {db}.{collection} ' +
+            r'\((?P<num>\d+) documents\)',
+            stderr,
+            re.MULTILINE,
+        )
+        if m:
+            stats.num_docs = int(m.group('num'))
+
+    finally:
+        process.terminate()
+
+
+def mongo_restore(
+    stream: BinaryIO,
+    uri: str,
+    collection: str,
+    db: str = None,
+    buffer_size=None,
+    drop=False,
+    cmd_prefix='',
+) -> MongoStats:
+    '''
+    Executes mongorestore, restoring a previously gzipped dump from the given
+    stream.
+
+    Args:
+    - stream: stream to read from
+    - uri: Mongo connection string
+    - collection: name of the collection to be restored
+    - db: name of the database (defaults to the one in the URI or 'admin')
+    - buffer_size: size of each chunk to be read (default: 10MB)
+    - drop: drop current collection
+    - cmd_prefix: prefix to be added to the mongorestore base command
+
+    Yields:
+        MongoStats
+    '''
+    buffer_size = buffer_size or 10_000_000
+
+    parts = parse_uri(uri)
+    db = db or parts.get('db', 'admin')
+    stats = MongoStats(db=db, collection=collection)
+
+    args = shell_split(cmd_prefix + 'mongorestore')
+    args += get_cmd_args(uri)
+    if drop:
+        args += ['--drop']
+    args += [
+        '--archive', '--gzip',
+        '--nsInclude', '*.*',
+        '--nsFrom', '$db$.$col$',
+        '--nsTo', f'{db}.{collection}',
+    ]
+
+    with stats.measure():
+        process = subprocess.run(
+            args,
+            errors=None,
+            stdin=stream,
+            text=True,
+            check=True,
+            bufsize=buffer_size,
+            capture_output=True,
+        )
+
+        num_match = re.search(
+            f'finished restoring {db}.{collection} ' +
+            r'\((?P<num>\d+) documents\)',
+            process.stderr,
+            re.MULTILINE,
+        )
+        stats.num_docs = int(num_match.group('num')) if num_match else None
+
+        dup_match = re.findall(
+            r"_id_ dup key: \{ : ObjectId\('(?P<id>[0-9a-fA-F]+)'\) \}",
+            process.stderr,
+            re.MULTILINE,
+        )
+        stats.duplicated_ids = [
+            ObjectId(id) for id in dup_match if id
+        ]
+        stats.num_docs -= len(stats.duplicated_ids)
+
+    return stats
---
+++ tests/test_mongo_utils.py
@@ -0,0 +1,236 @@
+from contextlib import contextmanager
+import io
+import logging
+import os
+import re
+import shutil
+import subprocess
+import tarfile
+import time
+import uuid
+import urllib.request
+
+import docker
+from hypothesis import given, strategies
+from pymongo import MongoClient
+import pytest
+from unittest.mock import MagicMock
+
+from lambda_mongo_utils import mongo_utils
+
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(os.getenv('LOG_LEVEL', 'INFO'))
+
+
+def to_utf8(s):
+    if hasattr(s, 'decode'):
+        return s.decode('utf-8')
+    else:
+        return str(s)
+
+
+@pytest.fixture(scope='function')
+def docker_container():
+    @contextmanager
+    def inner(image='mongo:4.0', appdir=None, **kwargs):
+        if appdir:
+            kwargs.setdefault('volumes', {
+                appdir: {
+                    'bind': '/app',
+                    'mode': 'rw',
+                },
+            })
+            kwargs.setdefault('working_dir', '/app')
+        client = docker.from_env()
+        container = client.containers.run(
+            image,
+            detach=True,
+            auto_remove=True,
+            remove=True,
+            **kwargs,
+        )
+        try:
+            yield container
+        finally:
+            container.kill()
+
+    return inner
+
+
+def mock_mongo_file(version, dest):
+    with tarfile.open(dest, 'w') as tar:
+        for util in mongo_utils.AVAILABLE_MONGO_UTILS:
+            cmd = f'echo -n {util} {version}'.encode('ascii')
+            tar_info = tarfile.TarInfo(f'mongo-xxx/bin/{util}')
+            tar_info.size = len(cmd)
+
+            tar.addfile(tar_info, io.BytesIO(cmd))
+
+
+@given(
+    utils=strategies.lists(
+        strategies.sampled_from(mongo_utils.AVAILABLE_MONGO_UTILS),
+        unique=True,
+    ),
+)
+def test_download_utils(tmp_path, monkeypatch, utils):
+
+    versions = {
+        '4.2-latest': 'http://downloads.mongodb.org/linux/mongodb-linux-x86_64-amazon2-v4.2-latest.tgz',  # noqa: E501
+        '4.0-latest': 'http://downloads.mongodb.org/linux/mongodb-linux-x86_64-amazon2-v4.0-latest.tgz',  # noqa: E501
+        '4.2.0': 'http://downloads.mongodb.org/linux/mongodb-linux-x86_64-amazon2-v4.2.0.tgz',            # noqa: E501
+    }
+
+    for version, version_url in versions.items():
+        mock = MagicMock()
+        monkeypatch.setattr(urllib.request, 'urlretrieve', lambda url, dest: (
+            mock_mongo_file(version, dest), mock(url),
+        ))
+        dest = tmp_path / 'tmp-download-utils' / version
+        shutil.rmtree(dest, ignore_errors=True)
+
+        r = mongo_utils.download_utils(dest=dest, version=version, utils=utils)
+        mock.assert_called_once_with(version_url)
+
+        util_paths = dest.glob('*')
+        assert set(u.name for u in util_paths) == set(utils)
+
+        for util, util_path in r.items():
+            process = subprocess.run(
+                ['sh', '-c', util_path],
+                capture_output=True,
+                check=True,
+                text=True,
+            )
+            assert f'{util} {version}' == process.stdout
+
+        shutil.rmtree(dest, ignore_errors=True)
+        with pytest.raises(ValueError):
+            mongo_utils.download_utils(
+                dest=dest, version=version, utils=['non-existing-util'])
+
+        dest = dest / f'binary--{uuid.uuid4()}'
+        with dest.open('w') as fp:
+            fp.write('testing...')
+
+        with pytest.raises(Exception) as e:
+            mongo_utils.download_utils(dest=dest, version=version)
+
+        assert re.search('not a directory', str(e.value))
+
+
+def wait_for_mongo_to_be_up(container, timeout=None):
+    '''
+    Waits until I can connect to Mongo inside the given container.
+    '''
+    rc = None
+    timeout = timeout or 10
+    t0 = time.time()
+    stderr = ''
+
+    while time.time() - t0 < timeout:
+        # Try again until Mongo connects
+        rc, (_, stderr) = container.exec_run(
+            ['mongo', '--eval', 'quit()'],
+            demux=True,
+        )
+        if rc == 0:
+            break
+        else:
+            time.sleep(1.0)
+
+    if rc != 0:
+        LOGGER.warning(to_utf8(stderr))
+
+    return rc == 0
+
+
+def test_mongo_dump_and_restore(docker_container, tmp_path):
+    # Dummy data insertion
+    docs = [
+        {'name': 'col1doc1'},
+        {'name': 'col1doc2'},
+        {'name': 'col1doc3'},
+    ]
+    inserted_doc_ids = None
+    port = 27020
+    client = MongoClient(f'mongodb://localhost:{port}')
+    uri = 'mongodb://localhost/tmpdb'
+    dump = str(tmp_path / 'dump1.tgz')
+
+    with docker_container('mongo:4.0', ports={'27017/tcp': str(port)}, appdir=str(tmp_path)) as container:  # noqa: E501
+        wait_for_mongo_to_be_up(container)
+        cmd_prefix = f'docker exec -i {container.id} '
+        inserted_doc_ids = client.db1['col1'].insert_many(docs).inserted_ids
+
+        with mongo_utils.mongo_dump(cmd_prefix=cmd_prefix, uri=uri, collection='col1', db='db1') as (stream, stats):  # noqa: E501
+            with open(dump, 'wb') as fp:
+                fp.write(stream.read())
+
+        assert stats.num_docs == 3
+
+        with mongo_utils.mongo_dump(cmd_prefix=cmd_prefix, uri=uri, collection='col1', db='db1', count=False) as (_, stats):  # noqa: E501
+            pass
+
+        assert not stats.num_docs
+
+        with pytest.raises(Exception) as exc:
+            with mongo_utils.mongo_dump(cmd_prefix=cmd_prefix + ' false ', uri=uri, collection='col1', db='db1') as _:  # noqa: E501
+                pass
+
+        assert re.search('exited with error code', str(exc))
+
+    with docker_container('mongo:4.0', ports={'27017/tcp': str(port)}, appdir=str(tmp_path)) as container:  # noqa: E501
+        wait_for_mongo_to_be_up(container)
+
+        def restore_dump(**kwargs):
+            with open(dump, 'rb') as fp:
+                return mongo_utils.mongo_restore(
+                    stream=fp,
+                    cmd_prefix=f'docker exec -i {container.id} ',
+                    uri=uri,
+                    collection='col2',
+                    db='db2',
+                    **kwargs,
+                )
+
+        # Insert one document and check if it wasn't overwritten
+        col = MongoClient(f'mongodb://localhost:{port}').db2['col2']
+        col.insert_one({
+            '_id': inserted_doc_ids[0],
+            'name': 'test',
+        })
+        stats = restore_dump()
+        assert {d['name'] for d in col.find()} == {
+            'test', 'col1doc2', 'col1doc3',
+        }
+        assert stats.num_docs == 2
+
+        # Checking if duplicated docs are properly returned
+        col.drop()
+        col.insert_one({
+            '_id': inserted_doc_ids[0],
+            'name': 'new doc 1',
+        })
+        col.insert_one({'name': 'new doc 2'})
+        stats = restore_dump()
+
+        assert stats.duplicated_ids == [inserted_doc_ids[0]]
+
+        r = col.delete_many({'_id': {'$in': stats.duplicated_ids}})
+        LOGGER.warning(r.raw_result)
+        stats = restore_dump()
+        assert stats.num_docs == 1
+        assert set(stats.duplicated_ids) == set(inserted_doc_ids[1:])
+        assert {d['name'] for d in col.find()} == {
+            'col1doc1', 'new doc 2', 'col1doc2', 'col1doc3',
+        }
+
+        # Now drop the collection
+        col.insert_one({'name': 'new doc'})
+        stats = restore_dump(drop=True)
+        assert {d['name'] for d in col.find()} == {
+            'col1doc1', 'col1doc2', 'col1doc3',
+        }
+        assert stats.num_docs == 3
